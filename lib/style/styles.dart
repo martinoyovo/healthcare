30a978bb2c3d1078672a7d6d598311694cff3feb
Martino Yovo <martinosoyen@gmail.com>
2021-05-23 02:08:35 +0200

    Text styles and colors added

---
+++ lib/style/styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:hack_app/utils/size_config.dart';
+
+Color primaryColor = Color(0xFF0E6535);
+Color greyColor = Color(0xFFE0E0E0);
+Color bgColor = Color(0xFFEEEEEE);
+
+final textStyle = TextStyle(fontSize: getProportionateScreenWidth(20));
