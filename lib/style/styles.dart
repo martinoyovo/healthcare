10b630335570030bed439190c40e9a642a20c38c
Martino Yovo <martinosoyen@gmail.com>
2021-05-27 16:17:53 +0200

    New styles added

--- lib/style/styles.dart
+++ lib/style/styles.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hack_app/utils/size_config.dart';
 
-Color primaryColor = Color(0xFF0E6535);
+Color primaryColor = Color(0xFF36B16C);
 Color greyColor = Color(0xFFE0E0E0);
-Color bgColor = Color(0xFFEEEEEE);
+Color bgColor = Color(0xFFF5F5F5);
 
 final textStyle = TextStyle(fontSize: getProportionateScreenWidth(20));
