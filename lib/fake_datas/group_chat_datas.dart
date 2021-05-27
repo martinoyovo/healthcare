f15d16a68bc7bc9139d1002dfc24f9ba75b9eda7
Martino Yovo <martinosoyen@gmail.com>
2021-05-27 16:14:28 +0200

    Group chat datas added

---
+++ lib/fake_datas/group_chat_datas.dart
@@ -0,0 +1,134 @@
+List<Map<String, dynamic>> groupChatDatas = [
+  {
+    "id": 1,
+    "text":
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text",
+    "from": {"id": 5, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "12/25/2020"
+  },
+  {
+    "id": 2,
+    "text":
+        "took a galley of type and scrambled it to make a type specimen book. It has survived not only",
+    "from": {"id": 5, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "12/5/2020"
+  },
+  {
+    "id": 3,
+    "text": "five centuries, but also the leap into electronic",
+    "from": {"id": 4, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "2/6/2021"
+  },
+  {
+    "id": 4,
+    "text": "It was popularised in the 1960s with the release",
+    "from": {"id": 4, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "2/23/2021"
+  },
+  {
+    "id": 5,
+    "text":
+        "of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop",
+    "from": {"id": 5, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "12/21/2020"
+  },
+  {
+    "id": 6,
+    "text":
+        "publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "from": {"id": 4, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "5/18/2021"
+  },
+  {
+    "id": 7,
+    "text": "It is a long established fact that a reader will be distracted",
+    "from": {"id": 5, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "12/13/2020"
+  },
+  {
+    "id": 8,
+    "text": "Lorem Ipsum is that it has a more-or-less normal",
+    "from": {"id": 4, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "2/3/2021"
+  },
+  {
+    "id": 9,
+    "text":
+        "distribution of letters, as opposed to using 'Content here, content here'",
+    "from": {"id": 5, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "9/21/2020"
+  },
+  {
+    "id": 10,
+    "text": "making it look like readable English. Many desktop",
+    "from": {"id": 5, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "5/27/2020"
+  },
+  {
+    "id": 11,
+    "text":
+        "publishing packages and web page editors now use Lorem Ipsum as their default model text",
+    "from": {"id": 4, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "7/25/2020"
+  },
+  {
+    "id": 12,
+    "text":
+        "nd a search for 'lorem ipsum' will uncover many web sites still in their infancy",
+    "from": {"id": 5, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "3/4/2021"
+  },
+  {
+    "id": 13,
+    "text":
+        "Various versions have evolved over the years, sometimes by accident, sometimes on purpose",
+    "from": {"id": 4, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "4/27/2021"
+  },
+  {
+    "id": 14,
+    "text":
+        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of",
+    "from": {"id": 4, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "9/22/2020"
+  },
+  {
+    "id": 15,
+    "text":
+        "classical Latin literature from 45 BC, making it over 2000 years old",
+    "from": {"id": 5, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "12/16/2020"
+  },
+  {
+    "id": 16,
+    "text": "Richard McClintock, a Latin professor at Hampden-Sydney",
+    "from": {"id": 5, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "12/28/2020"
+  },
+  {
+    "id": 17,
+    "text":
+        "College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites",
+    "from": {"id": 5, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "12/7/2020"
+  },
+  {
+    "id": 18,
+    "text":
+        "Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum",
+    "from": {"id": 4, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "2/23/2021"
+  },
+  {
+    "id": 19,
+    "text": "comes from a line in section 1.10.32.",
+    "from": {"id": 5, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "5/7/2021"
+  },
+  {
+    "id": 20,
+    "text": "There are many variations of passages of Lorem Ipsum available",
+    "from": {"id": 4, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "6/30/2020"
+  }
+];
