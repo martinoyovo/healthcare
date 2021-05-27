930b8d7b97427da2312be0f1743258fe51b1ba61
Martino Yovo <martinosoyen@gmail.com>
2021-05-27 16:14:04 +0200

    Doctor list file added

---
+++ lib/fake_datas/doctors_list.dart
@@ -0,0 +1,9 @@
+List<Map<String, dynamic>> dDatas = [
+  {
+    "id": 1,
+    "image": "assets/images/rating.png"
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text",
+    "from": {"id": 5, "name": "Docteur Yovo", "field": "Psychologie"},
+    "date": "12/25/2020"
+  },
+];
